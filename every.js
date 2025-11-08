d8d42ee9de86ad87c4d2f2df406d22f725de6663
Jeniffer Jojo <jenifferjojo@Jeniffers-MacBook-Pro.local>
2025-11-08 23:11:27 +0530

    Verify if all ages listed for an event are 18 or above

--- every.js
+++ every.js
@@ -14,6 +14,10 @@ const isPositive = function (rainfallData) {
   return rainfallData > 0;
 };
 
+const isAbove18 = function (age) {
+  return age > 18;
+};
+
 console.log("7 ->", [[22, 23], [25, 24, 22], [29]].flat().every(isBelow32));
 
 console.log("29 ->", [[3, 4], [5, 2], [1]].flat().every(isPositive));
@@ -21,3 +25,5 @@ console.log("29 ->", [[3, 4], [5, 2], [1]].flat().every(isPositive));
 console.log("33 ->", [[110, 115], [118], [109]].flat().every(isBelow120));
 
 console.log("54 ->", [[30, 43], [15, 24], [15]].flat().every(isBelow50));
+
+console.log("62 ->", [[30, 43], [37, 24], [19]].flat().every(isAbove18));
