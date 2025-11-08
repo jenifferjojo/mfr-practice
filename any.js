75db9b6e0d9c8a8b88ff7a88d4090dda6d026086
Jeniffer Jojo <jenifferjojo@Jeniffers-MacBook-Pro.local>
2025-11-08 23:27:22 +0530

    determine whether the note “fa” appears in any music sheet

--- any.js
+++ any.js
@@ -10,6 +10,10 @@ const anySangSo = function (notes) {
   return notes.includes("so");
 };
 
+const anySangFa = function (notes) {
+  return notes.includes("fa");
+};
+
 const isOver7 = function (measure) {
   return measure > 7;
 };
@@ -39,3 +43,5 @@ console.log("47 ->",[[2, 3], [1], [4, 2]].flat().some(isEquals4));
 console.log("53 ->",[["excellent", "to", "meet", "you"], ["how", "are", "you"]].some(anyWroteExcellent));
 
 console.log("61 ->",[[82, 67, 91], [91, 78], [84, 52]].flat().some(isOver90));
+
+console.log("12 ->",[["mi", "fa", "so"], ["do", "mi"], ["fa"]].some(anySangFa));
