572a1ebe24ec45146590a1a21d40ff8293aeb69f
Jeniffer Jojo <jenifferjojo@Jeniffers-MacBook-Pro.local>
2025-11-08 22:52:19 +0530

    check whether all recorded rainfall values are under 50

--- every.js
+++ every.js
@@ -6,6 +6,10 @@ const isBelow120 = function (unitsConsumed) {
   return unitsConsumed < 120;
 };
 
+const isBelow50 = function (rainfallData) {
+  return rainfallData < 50;
+};
+
 const isPositive = function (rainfallData) {
   return rainfallData > 0;
 };
@@ -15,3 +19,5 @@ console.log("7 ->", [[22, 23], [25, 24, 22], [29]].flat().every(isBelow32));
 console.log("29 ->", [[3, 4], [5, 2], [1]].flat().every(isPositive));
 
 console.log("33 ->", [[110, 115], [118], [109]].flat().every(isBelow120));
+
+console.log("54 ->", [[30, 43], [15, 24], [15]].flat().every(isBelow50));
