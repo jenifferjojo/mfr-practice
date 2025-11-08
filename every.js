65ae9e92e7758ec5d71d524498dbf09d9fdbabdd
Jeniffer Jojo <jenifferjojo@Jeniffers-MacBook-Pro.local>
2025-11-08 23:45:28 +0530

    verify whether every participant completed at least one task

--- every.js
+++ every.js
@@ -18,6 +18,10 @@ const isAbove18 = function (age) {
   return age > 18;
 };
 
+const isOver0 = function (taskDone) {
+  return taskDone > 0;
+};
+
 console.log("7 ->", [[22, 23], [25, 24, 22], [29]].flat().every(isBelow32));
 
 console.log("29 ->", [[3, 4], [5, 2], [1]].flat().every(isPositive));
@@ -27,3 +31,5 @@ console.log("33 ->", [[110, 115], [118], [109]].flat().every(isBelow120));
 console.log("54 ->", [[30, 43], [15, 24], [15]].flat().every(isBelow50));
 
 console.log("62 ->", [[30, 43], [37, 24], [19]].flat().every(isAbove18));
+
+console.log("72 ->", [[3, 2], [6, 1], [6, 4, 8]].flat().every(isOver0));
