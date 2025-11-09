a80dfad4a837a45b5a0eeda428cc844260f12e00
Jeniffer Jojo <jenifferjojo@Jeniffers-MacBook-Pro.local>
2025-11-09 13:05:38 +0530

    pulled into functions and function calls

--- unique.js
+++ unique.js
@@ -1,63 +1,182 @@
 const unique = function (alreadySeen, currentSeen) {
-  if (!alreadySeen.includes(currentSeen)) {
-    alreadySeen.push(currentSeen); 
+  const data = alreadySeen.slice();
+
+  if (!data.includes(currentSeen)) {
+    data.push(currentSeen); 
   }
     
-  return alreadySeen;
+  return data;
 };
 
-console.log("2 ->",[["Orion", "Leo"], ["Taurus"], ["Orion", "Gemini"]].flat().reduce(unique, []));
+const spottedConstellations = function() {
+  const data = [["Orion", "Leo"], ["Taurus"], ["Orion", "Gemini"]];
+  return data.flat().reduce(unique, []);
+}
 
-console.log("3 ->",["sparrow", "crow", "sparrow", "eagle", "crow"].reduce(unique, []));
+const spottedBirds = function() {
+  const data = ["sparrow", "crow", "sparrow", "eagle", "crow"];
+  return data.reduce(unique, []);
+}
 
-console.log("4 ->",[["Asha", "Ravi", "Neel"], ["Ravi"], ["Asha", "Meera"]].flat().reduce(unique, []));
+const studentsPresent = function() {
+  const data = [["Asha", "Ravi", "Neel"], ["Ravi"], ["Asha", "Meera"]];
+  return data.flat().reduce(unique, []);
+}
 
-console.log("9 ->",[["blue", "yellow"], ["yellow", "green"], ["blue"]].flat().reduce(unique, []));
+const colorsUsed = function() {
+  const data = [["blue", "yellow"], ["yellow", "green"], ["blue"]];
+  return data.flat().reduce(unique, []);
+}
 
-console.log("11 ->",[["rice", "lentils"], ["rice"], ["curd", "lentils"]].flat().reduce(unique, []));
+const ingredientsUsed = function() {
+  const data = [["rice", "lentils"], ["rice"], ["curd", "lentils"]];
+  return data.flat().reduce(unique, []);
+}
 
-console.log("14 ->",["small", "large", "medium", "small"].reduce(unique, []));
+const sizesUsed = function() {
+  const data = ["small", "large", "medium", "small"];
+  return data.reduce(unique, []);
+}
 
-console.log("16 ->",[[1, 2], [3], [2, 4, 1]].flat().reduce(unique, []));
+const chaptersCompleted = function() {
+  const data = [[1, 2], [3], [2, 4, 1]];
+  return data.flat().reduce(unique, []);
+}
 
-console.log("20 ->",[["apple", "banana"], ["apple"], ["apple", "orange"]].flat().reduce(unique, []));
+const fruitsUsed = function() {
+  const data = [["apple", "banana"], ["apple"], ["apple", "orange"]];
+  return data.flat().reduce(unique, []);
+}
 
-console.log("22 ->",[["Inception", "Dunkirk"], ["Interstellar"], ["Inception"]].flat().reduce(unique, []));
+const moviesWatched = function() {
+  const data = [["Inception", "Dunkirk"], ["Interstellar"], ["Inception"]];
+  return data.flat().reduce(unique, []);
+}
 
-console.log("23 ->",["A", "B", "A", "C", "B"].reduce(unique, []));
+const attendees = function() {
+  const data = ["A", "B", "A", "C", "B"];
+  return data.reduce(unique, []);
+}
 
-console.log("25 ->",[["rose", "lily"], ["lily", "tulip"]].flat().reduce(unique, []));
+const flowersUsed = function() {
+  const data = [["rose", "lily"], ["lily", "tulip"]];
+  return data.flat().reduce(unique, []);
+}
 
-console.log("27 ->",[["A", "B"], ["B", "C"], ["A"]].flat().reduce(unique, []));
+const stationsAnnounced = function() {
+  const data = [["A", "B"], ["B", "C"], ["A"]];
+  return data.flat().reduce(unique, []);
+}
 
-console.log("31 ->",[["idli", "vada"], ["vada", "upma"]].flat().reduce(unique, []));
+const snacksServed = function() {
+  const data = [["idli", "vada"], ["vada", "upma"]];
+  return data.flat().reduce(unique, []);
+}
 
-console.log("32 ->",[["sunset", "bird"], ["river"], ["sunset"]].flat().reduce(unique, []));
+const themes = function() {
+  const data = [["sunset", "bird"], ["river"], ["sunset"]];
+  return data.flat().reduce(unique, []);
+}
 
-console.log("36 ->",[["cheese", "bread"], ["tomato"], ["bread"]].flat().reduce(unique, []));
+const ingredientsNeeded = function() {
+  const data = [["cheese", "bread"], ["tomato"], ["bread"]];
+  return data.flat().reduce(unique, []);
+}
 
-console.log("37 ->",[["sky", "blue"], ["night"], ["sky", "dark"]].flat().reduce(unique, []));
+const wordsUsed = function() {
+  const data = [["sky", "blue"], ["night"], ["sky", "dark"]];
+  return data.flat().reduce(unique, []);
+}
 
-console.log("38 ->",[["toy", "sticker"], ["candy", "sticker"]].flat().reduce(unique, []));
+const giftItemsUsed = function() {
+  const data = [["toy", "sticker"], ["candy", "sticker"]];
+  return data.flat().reduce(unique, []);
+}
 
-console.log("42 ->",[["Tom", "Jerry"], ["Jerry", "Spike"]].flat().reduce(unique, []));
+const participants = function() {
+  const data = [["Tom", "Jerry"], ["Jerry", "Spike"]];
+  return data.flat().reduce(unique, []);
+}
 
-console.log("43 ->",[["Vega", "Sirius"], ["Vega", "Rigel"]].flat().reduce(unique, []));
+const starsSeen = function() {
+  const data = [["Vega", "Sirius"], ["Vega", "Rigel"]];
+  return data.flat().reduce(unique, []);
+}
 
-console.log("46 ->",[["mint", "ginger"], ["lemon"], ["mint"]].flat().reduce(unique, []));
+const uniqueFlavors = function() {
+  const data = [["mint", "ginger"], ["lemon"], ["mint"]];
+  return data.flat().reduce(unique, []);
+}
 
-console.log("48 ->",[["pencil", "charcoal"], ["ink"], ["pencil"]].flat().reduce(unique, []));
+const toolsUsed = function() {
+  const data = [["pencil", "charcoal"], ["ink"], ["pencil"]];
+  return data.flat().reduce(unique, []);
+}
 
-console.log("50 ->",[["salt", "pepper"], ["turmeric"], ["salt"]].flat().reduce(unique, []));
+const spicesUsed = function() {
+  const data = [["salt", "pepper"], ["turmeric"], ["salt"]];
+  return data.flat().reduce(unique, []);
+}
 
-console.log("55 ->",[["Inception", "Dunkirk"], ["Interstellar"], ["Inception"]].flat().reduce(unique, []));
+const moviesMentioned = function() {
+  const data = [["Inception", "Dunkirk"], ["Interstellar"], ["Inception"]];
+  return data.flat().reduce(unique, []);
+}
 
-console.log("57 ->",["sparrow", "crow", "sparrow", "eagle", "crow"].reduce(unique, []));
+const birdsSpotted = function() {
+  const data = ["sparrow", "crow", "sparrow", "eagle", "crow"];
+  return data.reduce(unique, []);
+}
 
-console.log("60 ->",[["chocolate", "vanilla"], ["strawberry", "vanilla"], ["chocolate"]].flat().reduce(unique, []));
+const flavoursAvailable = function() {
+  const data = [["chocolate", "vanilla"], ["strawberry", "vanilla"], ["chocolate"]];
+  return data.flat().reduce(unique, []);
+}
 
-console.log("63 ->",[["salt", "egg"], ["water", "salt"], ["egg"]].flat().reduce(unique, []));
+const recipeIngredients = function() {
+  const data = [["salt", "egg"], ["water", "salt"], ["egg"]];
+  return data.flat().reduce(unique, []);
+}
 
-console.log("68 ->",[["olives", "cheese"], ["cheese", "pepperoni"], ["olives"]].flat().reduce(unique, []));
+const toppingsChoosed = function() {
+  const data = [["olives", "cheese"], ["cheese", "pepperoni"], ["olives"]];
+  return data.flat().reduce(unique, []);
+}
 
-console.log("70 ->",[["red", "green"], ["green", "blue"], ["yellow", "red"]].flat().reduce(unique, []));
+const colorsSwatched = function() {
+  const data = [["red", "green"], ["green", "blue"], ["yellow", "red"]];
+  return data.flat().reduce(unique, []);
+}
+
+const main = function() {
+  console.log("2 => ", spottedConstellations());
+  console.log("3 => ", spottedBirds());
+  console.log("4 => ", studentsPresent());
+  console.log("9 => ", colorsUsed());
+  console.log("11 => ", ingredientsUsed());
+  console.log("14 => ", sizesUsed());
+  console.log("16 => ", chaptersCompleted());
+  console.log("20 => ", fruitsUsed());
+  console.log("22 => ", moviesWatched());
+  console.log("23 => ", attendees());
+  console.log("25 => ", flowersUsed());
+  console.log("27 => ", stationsAnnounced());
+  console.log("31 => ", snacksServed());
+  console.log("32 => ", themes());
+  console.log("36 => ", ingredientsNeeded());
+  console.log("37 => ", wordsUsed());
+  console.log("38 => ", giftItemsUsed());
+  console.log("42 => ", participants());
+  console.log("43 => ", starsSeen());
+  console.log("46 => ", uniqueFlavors());
+  console.log("48 => ", toolsUsed());
+  console.log("50 => ", spicesUsed());
+  console.log("55 => ", moviesMentioned());
+  console.log("57 => ", birdsSpotted());
+  console.log("60 => ", flavoursAvailable());
+  console.log("63 => ", recipeIngredients());
+  console.log("68 => ", toppingsChoosed());
+  console.log("70 => ", colorsSwatched());
+}
+
+main();
