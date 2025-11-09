e29ba51d0a07a2b1d32432731af79c9b25fede8e
Jeniffer Jojo <jenifferjojo@Jeniffers-MacBook-Pro.local>
2025-11-09 10:47:20 +0530

    modified into function calls

--- every.js
+++ every.js
@@ -10,7 +10,7 @@ const isBelow50 = function (rainfallData) {
   return rainfallData < 50;
 };
 
-const isPositive = function (rainfallData) {
+const isAbove0 = function (rainfallData) {
   return rainfallData > 0;
 };
 
@@ -22,14 +22,43 @@ const isOver0 = function (taskDone) {
   return taskDone > 0;
 };
 
-console.log("7 ->", [[22, 23], [25, 24, 22], [29]].flat().every(isBelow32));
+const allBelow32 = function() {
+  const data = [[22, 23], [25, 24, 22], [29]];
+  return data.flat().every(isBelow32);
+}
 
-console.log("29 ->", [[3, 4], [5, 2], [1]].flat().every(isPositive));
+const allPositive = function() {
+  const data = [[3, 4], [5, 2], [1]];
+  return data.flat().every(isAbove0);
+}
 
-console.log("33 ->", [[110, 115], [118], [109]].flat().every(isBelow120));
+const allBelow120 = function() {
+  const data = [[110, 115], [118], [109]];
+  return data.flat().every(isBelow120);
+}
 
-console.log("54 ->", [[30, 43], [15, 24], [15]].flat().every(isBelow50));
+const allBelow50 = function() {
+  const data = [[30, 43], [15, 24], [15]];
+  return data.flat().every(isBelow50);
+}
 
-console.log("62 ->", [[30, 43], [37, 24], [19]].flat().every(isAbove18));
+const allAbove18 = function() {
+  const data = [[30, 43], [37, 24], [19]];
+  return data.flat().every(isAbove18);
+}
 
-console.log("72 ->", [[3, 2], [6, 1], [6, 4, 8]].flat().every(isOver0));
+const allOver0 = function() {
+  const data = [[3, 2], [6, 1], [6, 4, 8]];
+  return data.flat().every(isOver0);
+}
+
+const main = function() {
+  console.log("7 => ", allBelow120());
+  console.log("29 => ", allBelow32());
+  console.log("33 => ", allBelow50());
+  console.log("54 => ", allPositive());
+  console.log("62 => ", allOver0());
+  console.log("72 => ", allAbove18());
+}
+
+main();
