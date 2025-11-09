8189190ae111763831fd28f522c378e5cfd73783
Jeniffer Jojo <jenifferjojo@Jeniffers-MacBook-Pro.local>
2025-11-09 13:35:19 +0530

    modified function names

--- every.js
+++ every.js
@@ -22,7 +22,7 @@ const isOver0 = function (taskDone) {
   return taskDone > 0;
 };
 
-const allBelow32 = function() {
+const allTemperatureBelow32 = function() {
   const data = [[22, 23], [25, 24, 22], [29]];
   return data.flat().every(isBelow32);
 }
@@ -32,12 +32,12 @@ const allPositive = function() {
   return data.flat().every(isAbove0);
 }
 
-const allBelow120 = function() {
+const allUnitConsumedBelow120 = function() {
   const data = [[110, 115], [118], [109]];
   return data.flat().every(isBelow120);
 }
 
-const allBelow50 = function() {
+const allRainfallDataBelow50 = function() {
   const data = [[30, 43], [15, 24], [15]];
   return data.flat().every(isBelow50);
 }
@@ -53,9 +53,9 @@ const allOver0 = function() {
 }
 
 const main = function() {
-  console.log("7 => ", allBelow120());
-  console.log("29 => ", allBelow32());
-  console.log("33 => ", allBelow50());
+  console.log("7 => ", allUnitConsumedBelow120());
+  console.log("29 => ", allTemperatureBelow32());
+  console.log("33 => ", allRainfallDataBelow50());
   console.log("54 => ", allPositive());
   console.log("62 => ", allOver0());
   console.log("72 => ", allAbove18());
